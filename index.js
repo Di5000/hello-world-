816d85e49f03586cf690fd489b9daa1ad149fb8a
Di5000 <127889999+Di5000@users.noreply.github.com>
2024-12-16 18:23:46 +0300

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(1);
+
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function(){
+  output.innerHTML = '200 mln.cap soon';
+});
